2e6e5a4eb54eb2bd00a01b2767195471f84b0365
hukl <contact@smyck.org>
2011-12-22 16:50:24 +0100

    removed obsolete code and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,18 +28,6 @@ class User < ActiveRecord::Base
     ["workshifts.user_id = users.id AND workshifts.state <> 'cleared'"]
   )
 
-  # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
-  #
-  # uff.  this is really an authorization, not authentication routine.
-  # We really need a Dispatch Chain here or something.
-  # This will also let us return a human error message.
-  #
-  def self.authenticate(login, password)
-    return nil if login.blank? || password.blank?
-    u = find_by_login(login.downcase) # need to get the salt
-    u && u.authenticated?(password) ? u : nil
-  end
-
   def login=(value)
     write_attribute :login, (value ? value.downcase : nil)
   end
